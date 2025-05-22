6350d28af3452bf4d84471bae0d3977506ce337b
EsnaNurYlmz <esnanuryilmaz407@gmail.com>
2025-05-22 16:09:08 +0300

    Initial Commit

---
+++ JetGas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JetGas
+//
+//  Created by Esna nur Yılmaz on 22.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ JetGasTests/JetGasTests.swift
@@ -0,0 +1,17 @@
+//
+//  JetGasTests.swift
+//  JetGasTests
+//
+//  Created by Esna nur Yılmaz on 22.05.2025.
+//
+
+import Testing
+@testable import JetGas
+
+struct JetGasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
